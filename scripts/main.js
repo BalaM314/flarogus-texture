e90e51dfdde461c09368ef27e79623639358bb3c
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-11-25 16:41:42 +0530

    fix js skill issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 Events.on(EventType.ContentInitEvent, cons(() => {
-	const Engine = UnitTypes.UnitEngine;
-	UnitTypes.flare.engines = new Seq(new Engine(-2, -8, 4, -90), new Engine(2, -8, 4, -90));
+	const Engine = UnitType.UnitEngine;
+	UnitTypes.flare.engines = new Seq([new Engine(-2, -8, 4, -90), new Engine(2, -8, 4, -90)]);
 }));
